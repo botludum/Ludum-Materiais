9d196c75e1b9b4cbbb6ea6fa08ad73a1fa9fbe6c
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-06-20 14:15:40 -0300

    Populando banco de dados com links

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+
 let app = express();
 
 let apiRoutes = require("./routes/api-routes")
 
-// Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
 var corsMiddleware = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
 
@@ -21,19 +21,33 @@ var corsMiddleware = function(req, res, next) {
 
 app.use(corsMiddleware);
 
-// Connect to Mongoose and set connection variable
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost/ludum-materiais');
 
 var db = mongoose.connection;
 var port = process.env.PORT || 8081;
 
-// Send message for default URL
+db.collection('links').insertMany([
+    // MongoDB adds the _id field with an ObjectId if _id is not present
+    { title: "Página oficial da Pygame", type: "Site",link: "https://www.pygame.org/news", status: "S" },
+    { title: "Criando um Snake do zero com Pygame em 5 minutos (ou mais)", type: "Vídeo",link: "https://www.youtube.com/watch?v=H4TXHI9BRCQ", status: "S" }
+ ])
+ .then(function(result) {
+   console.log("OK!", result);
+ })
+
+db.collection('links').insertMany([
+    // MongoDB adds the _id field with an ObjectId if _id is not present
+    { title: "Página oficial da Pygame", type: "Site",link: "https://www.pygame.org/news", status: "S" },
+    { title: "Criando um Snake do zero com Pygame em 5 minutos (ou mais)", type: "Vídeo",link: "https://www.youtube.com/watch?v=H4TXHI9BRCQ", status: "S" }
+])
+ .then(function(result) {
+    console.log("Banco populado!")
+})
+ 
 app.get('/', (req, res) => res.send('Ludum Materiais'));
 
-// Use Api routes in the App
 app.use('/api', apiRoutes)
 
-// Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running on port " + port);
 });
